09e1f2459c2498d9f5d61319454dfa032eda33c7
Nate Deakers <ndeakers@gmail.com>
2021-05-03 12:20:25 -0700

    switching

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
 """Flask app for Cupcakes"""
+from flask import Flask, url_for, render_template, redirect, flash, jsonify
+from models import db, connect_db, Cupcake
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = "secret"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///cupcakes"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+
+connect_db(app)
+db.create_all()
--- models.py
+++ models.py
@@ -1,1 +1,26 @@
 """Models for Cupcake app."""
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+def connect_db(app):
+    """Connect this database to provided Flask app.
+    You should call this in your Flask app.
+    """
+    db.app = app
+    db.init_app(app)
+    
+    
+class Cupcake(db.Model):
+    "A cupcake"
+
+    __tablename__ = "cupcakes"
+
+    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    flavor = db.Column(db.String(30), nullable=False)
+    size = db.Column(db.String(30), nullable=False)
+    rating = db.Column(db.Integer, nullable=False)
+    image = db.Column(db.Text, nullable=False, default='https://tinyurl.com/demo-cupcake')
+
+
